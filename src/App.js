b26c5d2d7edb5003aa891744967767e8b64fbd37
rajputd <dileep_rajput@yahoo.com>
2018-11-24 12:03:29 -0500

    Add SocialBar to app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
 import NavMenu from './components/NavMenu';
+import ParticleContainer from './components/ParticleContainer';
+import SocialBar from './components/SocialBar';
 
 import './scss/App.scss';
-import ParticleContainer from './components/ParticleContainer';
+
+//get font awesome icon components
+import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
+//control what icons and links SocialBar will display
+const socialBarConfig = [
+  {
+    icon: faGithubSquare,
+    link: "https://github.com/rajputd/",
+  },
+  {
+    icon: faLinkedin,
+    link: "https://www.linkedin.com/in/dileep-rajput/",
+  },
+]
 
 class App extends Component {
   render() {
@@ -10,6 +26,11 @@ class App extends Component {
       <div className="App">
         <ParticleContainer />
         <NavMenu />
+        <section id="home">
+          <h1>Dileep Rajput</h1>
+          <h4>Software Engineer</h4>
+          <SocialBar socialButtons={socialBarConfig} />
+        </section>
       </div>
     );
   }
