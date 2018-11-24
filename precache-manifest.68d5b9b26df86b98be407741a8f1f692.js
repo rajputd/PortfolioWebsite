99abbaf5895ea29b9670dc0a16981873d2343ef2
rajputd <dileep_rajput@yahoo.com>
2018-11-23 19:00:32 -0500

    Updates

---
+++ precache-manifest.68d5b9b26df86b98be407741a8f1f692.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4dab0bcb6bdca316350b",
+    "url": "/static/css/main.e35a038f.chunk.css"
+  },
+  {
+    "revision": "4dab0bcb6bdca316350b",
+    "url": "/static/js/main.4dab0bcb.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "99b49ff2973e78b9aadddd69411672b6",
+    "url": "/index.html"
+  }
+];
