1201a4ae6b401a5d9661857f3f6df75a169a1fc7
rajputd <dileep_rajput@yahoo.com>
2018-11-25 17:48:57 -0500

    Replace NavBar react-bootstrap with regular bootstrap

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,34 +1,30 @@
 import React from 'react';
-import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
 import '../scss/NavMenu.scss';
 
 const NavMenu = (props) => {
 	return (
-		<Navbar collapseOnSelect id="NavMenu">
-			<Navbar.Header>
-				<Navbar.Brand>
-					<a href="#brand">Dileep Rajput</a>
-				</Navbar.Brand>
-				<Navbar.Toggle />
-			</Navbar.Header>
-			<Navbar.Collapse>
-				<Nav pullRight>
-					<NavItem eventKey={1} href="#">
-						About
-          </NavItem>
-					<NavItem eventKey={2} href="#">
-						Experience
-          </NavItem>
-					<NavItem eventKey={3} href="#">
-						Portfolio
-          </NavItem>
-					<NavItem eventKey={4} href="#">
-						Contact
-          </NavItem>
-				</Nav>
-			</Navbar.Collapse>
-		</Navbar>
+		<nav id="NavMenu" className="navbar navbar-default">
+		<div className="container-fluid">
+			<div className="navbar-header">
+				<button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+					<span className="sr-only">Toggle navigation</span>
+					<span className="icon-bar"></span>
+					<span className="icon-bar"></span>
+					<span className="icon-bar"></span>
+				</button>
+				<a className="navbar-brand">Dileep Rajput</a>
+			</div>
+
+			<div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul className="nav navbar-nav navbar-right">
+					<li><a>About</a></li>
+					<li><a>Portfolio</a></li>
+					<li><a>Contact</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
 	);
 }
 
