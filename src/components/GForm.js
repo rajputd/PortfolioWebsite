ede4a0ccbd1a88e2836d8ceda96002a578e81cbd
rajputd <dileep_rajput@yahoo.com>
2018-11-25 16:35:56 -0500

    Display submission status when form is submitted

--- src/components/GForm.js
+++ src/components/GForm.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import '../scss/GForm.scss';
+
 class GForm extends Component {
   constructor(props, context) {
     super(props, context);
@@ -8,11 +10,13 @@ class GForm extends Component {
     this.handleEmailChange = this.handleEmailChange.bind(this);
     this.handleMessageChange = this.handleMessageChange.bind(this);
     this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.setSubmit = this.setSubmit.bind(this);
 
     this.state = {
       name: '',
       email: '',
       message: '',
+      submitSuccessful: null,
     };
   }
 
@@ -28,6 +32,10 @@ class GForm extends Component {
     this.setState({message: e.target.value});
   }
 
+  setSubmit(bool) {
+    this.setState({submitSuccessful: bool});
+  }
+
   handleFormSubmit(e) {
     //prevent form from redirecting to different page
     e.preventDefault();
@@ -35,9 +43,15 @@ class GForm extends Component {
     const url = "https://script.google.com/macros/s/AKfycbyVREX2GBuPbU6VcTEDbQ_wkxIffWqHfuY7KJc7/exec";
 
     //construct object to hold form data
+    let formFields = {
+      name: this.state.name,
+      email: this.state.email,
+      message: this.state.message,
+    };
+
     let data = [];
-    for(let field in this.state) {
-      data.push(encodeURIComponent(field) + '=' + encodeURIComponent(this.state[field]));
+    for(let field in formFields) {
+      data.push(encodeURIComponent(field) + '=' + encodeURIComponent(formFields[field]));
     }
 
     //convert object into a properly formated URI component
@@ -45,68 +59,82 @@ class GForm extends Component {
 
     let xhr = new XMLHttpRequest();
 
+    //create closure to change form submit state in xhr event listeners
+    const setSubmit = this.setSubmit;
+
     // Define what happens on successful data submission
     xhr.addEventListener('load', function(event) {
-      //TODO make thank you message appear
-      console.log('Yeah! Data sent and response loaded.');
-      console.log(event);
+      setSubmit(true);
     });
 
     // Define what happens in case of error
     xhr.addEventListener('error', function(event) {
-      //TODO make error message appear
-      console.log('Oops! Something goes wrong.');
-      console.log(event);
+      setSubmit(false);
     });
 
     //send data
     xhr.open('POST', url);
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     xhr.send(data);
-
-
-
   }
 
   render() {
 
-    return (
-      <form id="gform" method="POST">
-        <div className="form-group">
-          <label>Name</label>
-          <input
-            type="text"
-            className="form-control"
-            name="name"
-            value={this.state.name}
-            onChange={this.handleNameChange}
-          />
-        </div>
-        <div className="form-group">
-          <label>Email</label>
-          <input
-            type="email"
-            className="form-control"
-            name="email"
-            value={this.state.email}
-            onChange={this.handleEmailChange}
-          />
-        </div>
-        <div className="form-group">
-          <label>Message</label>
-          <textarea
-            className="form-control"
-            name="message"
-            value={this.state.message}
-            onChange={this.handleMessageChange}
-          ></textarea>
-        </div>
+    let submitMessage = "";
+    let disableSubmit = false;
 
-        <button
-          className="btn"
-          onClick={this.handleFormSubmit}
-          >Submit</button>
-      </form>
+    if (this.state.submitSuccessful === true){
+      submitMessage = "Thanks! I'll be in contact with you soon.";
+      disableSubmit = true;
+    }
+
+    if (this.state.submitSuccessful === false) {
+      submitMessage = "Oops! This is embarrassing. Something seems to have gone wrong. Please try again later.";
+    }
+
+    return (
+      <div>
+        <form>
+          <div className="form-group">
+            <label>Name</label>
+            <input
+              type="text"
+              className="form-control"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleNameChange}
+            />
+          </div>
+          <div className="form-group">
+            <label>Email</label>
+            <input
+              type="email"
+              className="form-control"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleEmailChange}
+            />
+          </div>
+          <div className="form-group">
+            <label>Message</label>
+            <textarea
+              className="form-control"
+              name="message"
+              value={this.state.message}
+              onChange={this.handleMessageChange}
+            ></textarea>
+          </div>
+
+          <button
+            className="btn"
+            onClick={this.handleFormSubmit}
+            disabled={disableSubmit}
+            >Submit
+          </button>
+        </form>
+
+        <p className="form-submit-message">{submitMessage}</p>
+      </div>
     );
   }
 }
